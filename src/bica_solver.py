1f09b40599ecb795015935914286f44fe736811c
Mikel <115263635+LDRMoliner@users.noreply.github.com>
2025-09-06 18:47:31 +0200

    Update bica_solver.py

--- src/bica_solver.py
+++ src/bica_solver.py
@@ -6,12 +6,12 @@
 ##  Creado por Mikel Molina.
 #=========================================================================================0
 
-
 from __future__ import print_function
+import sys
+sys.path.insert(0, 'bica')
 from forqes import Forqes
 from primer import Primer
 import socket
-import sys
 from file_parser import QBF
 import atexit
 import os
@@ -19,12 +19,13 @@ import signal
 
 def create_negation(pfname, temp_files):
     """
-        Encodes the negation of the input CNF formula.
+        Función que codifica la negación de la fórmula CNF de entrada.
+        :pfname: Nombre del archivo de la fórmula CNF.
+        :temp_files: Lista de archivos temporales para limpiar al final o cuando se interrumpe el proceso.
     """
 
     nofv = 0
     cnf_pos = []
-    # print(pfname)
     comment_pos = -1  # there is no line 'c n orig vars' in the file
     for i, line in enumerate(open(pfname, 'r')):
         if line[0] != 'p' and line[0] != 'c':
@@ -85,36 +86,53 @@ def create_negation(pfname, temp_files):
         sys.exit(1)
     return pos, neg
 
-def bica(file_name):
+def bica(file_name, temp_files):
     """
-        Converts a CNF formula to DNF using Bica.
+        Función que ejecuta el algoritmo BICA para convertir una fórmula CNF a DNF.
+        :param file_name: Nombre del archivo que contiene la fórmula CNF.
+        :param temp_files: Lista de archivos temporales para limpiar al final o cuando se interrumpe el proceso.
+        :return: Una cadena que representa la DNF en formato DIMACS.
     """
 
     fns = [file_name]
 
-    temp_files = []
     setup_execution(temp_files)
 
+    # Primero, se crea la negación de la fórmula CNF de entrada para facilitar el proceso de conversión a DNF.
     fns = list(create_negation(fns[0], temp_files))
 
+    # Se obtienen los primos de la fórmula CNF.
     primer = Primer('b', fns[0], fns[1], 0, True)
     primes = primer.run()
 
     fns[0], fns[1] = fns[1], fns[0]
 
+    # Se ejecuta el algoritmo Forqes para convertir la fórmula CNF a DNF con los primos obtenidos.
     miner = Forqes('forqes', fns[0], fns[1], primes, False, False, 0)
     mincnf, nofv = miner.run()
+
+    # Se construye la salida en formato DIMACS para la DNF resultante.
     lines = 'p dnf {} {}'.format(nofv, len(mincnf))
     for clid in mincnf:
         lines += '\n' + ' '.join([str(-int(l)) for l in primes[clid].split()])
     return lines
 
 def at_exit(temp_files):
+    """
+    Functión que se ejecuta al finalizar el programa o al recibir una señal de interrupción. Limpia los archivos temporales creados durante la ejecución.
+    :param temp_files:
+    :return:
+    """
     for file_name in temp_files:
         if os.path.exists(file_name):
             os.remove(file_name)
 
 def setup_execution(temp_files):
+    """
+    Configura el manejo de señales y la limpieza de archivos temporales al finalizar la ejecución del programa.
+    :param temp_files:
+    :return:
+    """
     def handler(signum, frame):
         sys.exit(0)
     signal.signal(signal.SIGINT, handler)
@@ -122,23 +140,41 @@ def setup_execution(temp_files):
     atexit.register(at_exit, temp_files)
 
 class BicaSolver(object):
+    """
+    Clase que implementa el Razonador con BICA para resolver fórmulas QBF.
+    """
     def __init__(self, qbf: QBF):
+        """
+        Constructor de la clase BicaSolver.
+        :param qbf:
+        """
         self.qbf = qbf
 
     def solve(self):
+        """
+        Método que resuelve la QBF utilizando el algoritmo BICA.
+        :return:
+        """
+        # Primero, se eliminan las tautologías de la QBF.
         self.qbf.clauses = [set(clause) for clause in self.qbf.clauses if all(lit in clause and -lit not in clause for lit in clause)]
         temp_files = []
         setup_execution(temp_files)
         while self.qbf.quantifiers and self.qbf.clauses:
+            """
+            Se procesa el bloque de cuantificadores más interno y se extraen las variables.
+            """
             block = self.qbf.quantifiers.pop()
             quantifier = block[0]
             variables = block[1]
             if quantifier == 'a':
+                """
+                Si el bloque actual es universal y es una DNF.
+                """
                 if not self.qbf.is_cnf:
                     dimacs_file_name = "{}_to_dimacs.cnf".format(self.qbf.file_name)
                     temp_files.append(dimacs_file_name)
                     self.qbf.write_file(dimacs_file_name, negate = True)
-                    dnf = bica(dimacs_file_name)
+                    dnf = bica(dimacs_file_name, temp_files)
                     self.qbf.clauses = []
                     dnf = dnf.split('\n')
                     for line in dnf:
@@ -146,6 +182,9 @@ class BicaSolver(object):
                     self.qbf.is_cnf = True
                     os.remove(dimacs_file_name)
                     temp_files.remove(dimacs_file_name)
+                """
+                Si el bloque actual es universal y es una CNF.
+                """
                 new_clauses = []
                 for index in range(len(self.qbf.clauses)):
                     new_clause = set([lit for lit in self.qbf.clauses[index] if abs(lit) not in variables])
@@ -155,17 +194,23 @@ class BicaSolver(object):
                 self.qbf.clauses = new_clauses
                 continue
             if quantifier == 'e':
+                """
+                Si el bloque actual es existencial y es una CNF.
+                """
                 if self.qbf.is_cnf:
                     dimacs_file_name = "{}_to_dimacs.cnf".format(self.qbf.file_name)
                     temp_files.append(dimacs_file_name)
                     self.qbf.write_file(dimacs_file_name)
-                    dnf = bica(dimacs_file_name)
+                    dnf = bica(dimacs_file_name, temp_files)
                     self.qbf.clauses = []
                     dnf = dnf.split('\n')
                     for line in dnf:
                         self.qbf.read_line(line)
                     os.remove(dimacs_file_name)
                     temp_files.remove(dimacs_file_name)
+                """
+                Si el bloque actual es existencial y es una DNF.
+                """
                 new_clauses = []
                 for index in range(len(self.qbf.clauses)):
                     new_clause = set([lit for lit in self.qbf.clauses[index] if abs(lit) not in variables])
@@ -174,6 +219,9 @@ class BicaSolver(object):
                     new_clauses.append(new_clause)
                 self.qbf.clauses = new_clauses
                 continue
+        """
+        Si no quedan términos en la DNF, se devuelve False. True de lo contrario.
+        """
         if not self.qbf.clauses and not self.qbf.is_cnf:
             return False
         return True
